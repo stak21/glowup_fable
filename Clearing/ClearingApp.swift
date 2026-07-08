0808ab2d25734c98a82b8157e927d7007d672ae8
shoji takashima <stakashima21@gmail.com>
2026-07-07 18:06:01 -0700

    Initial Commit

---
+++ Clearing/ClearingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearingApp.swift
+//  Clearing
+//
+//  Created by Shoji Takashima on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ClearingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
